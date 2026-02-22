c14f9d348ab182d93643cf66347467ce1c5195ee
mohamedalilab <mohamedali4dev@gmail.com>
2026-02-22 14:32:47 +0200

    first commit

--- src/roles/Users/Pages/Layout.jsx
+++ src/roles/Users/Pages/Layout.jsx
@@ -1,19 +1,16 @@
-
-import { Outlet } from 'react-router-dom';
-import Navbar from '../Components/Navbar';
-import Footer from '../Components/Footer';
+import { Outlet } from "react-router-dom";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
 
 const Layout = () => {
   return (
-    // <div className='app flex flex-col'>
-    <>
+    <div className="app flex flex-col">
       <Navbar />
-      <main className='grow'>
-        <Outlet /> 
+      <main className="grow">
+        <Outlet />
       </main>
       <Footer />
-    </>
-    // </div>
+    </div>
   );
 };
 
